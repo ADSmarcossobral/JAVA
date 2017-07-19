0cab700533d1d777f59173b332449ccfd3e174c0
Sobral <marcossobral94@gmail.com>
2017-07-19 19:44:56 -0300

    Segundo

---
+++ TesteNoSensor.java
@@ -0,0 +1,11 @@
+
+public class TesteNoSensor {
+
+	public static void main(String args[]){
+		NoSensor no1 = new NoSensor("1",54,23,9);
+		NoSensor no2 = new NoSensor("2",46,12,6);
+		
+		System.out.println(no1);
+		System.out.println(no2);
+	}
+}
