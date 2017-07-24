8dd1dd27736ce7f3065cf338096487ec92e3e70f
ADSmarcossobral <marcossobral94@gmail.com>
2017-07-24 17:07:15 -0300

    code

---
+++ Codando I/AreaMonitorada.java
@@ -0,0 +1,23 @@
+
+public class AreaMonitorada{
+
+    private NoSensor[] sensores;
+
+    public double amplitudeTermica(){
+        double maior = 0.0, menor = 0.0;
+        if(this.sensores.length > 0){
+            maior = sensores[0].getTemperatura();
+            menor = sensores[0].getTemperatura();
+            for(NoSensor sensor : this.sensores){
+                if(this.sensores.getTemperatura() > maior)
+                    maior = this.sensores.getTemperatura();
+                if(this.sensores.getTemperatura() < menor)
+                    menor = this.sensores.getTemperatura();
+            }
+        }
+        return maior - menor;
+    }
+
+    
+
+}
--- Codando I/TesteNoSensor.java
+++ Codando I/TesteNoSensor.java
@@ -2,12 +2,16 @@
 public class TesteNoSensor {
 
 	public static void main(String args[]){
+		// Trabalhando com arrays
 		NoSensor [][] nos;
-		nos = new NoSensor[2][2];
+		nos = new NoSensor[5][];
+
+		for(int x = 0; x < nos.length; x++)
+			nos[x] = new NoSensor[x];
+
 		for(int x = 0; x < nos.length; x++)
 			for(int y = 0; y < nos[x].length; y++)
 				nos[x][y] = new NoSensor();
 		
-
 	}
 }
