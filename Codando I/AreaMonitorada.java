f227c01798ac24abb3ed1f50b53b9e8d196faf74
ADSmarcossobral <marcossobral94@gmail.com>
2017-07-24 17:12:57 -0300

    code

--- Codando I/AreaMonitorada.java
+++ Codando I/AreaMonitorada.java
@@ -18,6 +18,19 @@ public class AreaMonitorada{
         return maior - menor;
     }
 
-    
+    public double amplitudeTermica(double altitude){
+        double maior = 0.0, menor = 0.0;
+        if(this.sensores.length > 0){
+            maior = sensores[0].getTemperatura();
+            menor = sensores[0].getTemperatura();
+            for(NoSensor sensor : this.sensores){
+                if(this.sensores.getTemperatura() > maior && this.sensores.getAltitude < altitude)
+                    maior = this.sensores.getTemperatura();
+                if(this.sensores.getTemperatura() < menor && this.sensores.getAltitude < altitude)
+                    menor = this.sensores.getTemperatura();
+            }
+        }
+        return maior - menor;
+    }
 
 }
