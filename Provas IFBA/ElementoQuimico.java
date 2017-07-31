8f940b4397ff526235f04b08df17ca33ad65af1a
aluno <aluno@SSA-D-CPIL03-04.intranet.cefetba.br>
2017-07-31 19:36:50 -0300

    up

--- Provas IFBA/ElementoQuimico.java
+++ Provas IFBA/ElementoQuimico.java
@@ -1,10 +1,51 @@
 
 public class ElementoQuimico{
-        private int numeroAtomico;
-        private String nome;
-        private int numeroMassa;
-        private int[] eletrons;
+      private int numeroAtomico;
+      private String nome;
+      private int numeroMassa;
+      private int[] eletrons;
+      
+      public ElementoQuimico(int numeroAtomico, int numeroMassa, String nome){
+            this.setNumeroAtomico(numeroAtomico);
+            this.setNumeroMassa(numeroMassa);
+            this.setNome(nome);
+            this.eletrons = new int[7];
+      }
 
+      public int getNumeroAtomico(){
+            return this.numeroAtomico;
+      }
 
+      public void setNumeroAtomico(int numeroAtomico){
+            this.numeroAtomico = numeroAtomico;
+      }
+
+      public int getNumeroMassa(){
+          return this.numeroMassa;
+      }
+
+      public void setNumeroMassa(){
+          this.numeroMassa = numeroMassa;
+      }
+
+      public String getNome(){
+          return this.nome;
+      }
+
+      public void setNome(String nome){
+          this.nome = nome;
+      }
+
+      public boolean equals(ElementoQuimico eq){
+          return this.getNumeroAtomico() = eq.getNumeroAtomico();
+      }
+
+      public int getQtdeEletrons(int camada){
+          return this.eletrons[camada];
+      }
+
+      public void setQTdeEletrons(int camada, int qtdeEletrons){
+          this.eletrons[camada] = qtdeEletrons;
+      }
 
 }
