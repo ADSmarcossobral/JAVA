adf0c098f4ca47a50ff2cd26776e74fc3bfa912a
aluno <aluno@SSA-D-CPIL03-04.intranet.cefetba.br>
2017-08-01 20:38:00 -0300

    update

--- Provas IFBA/2013-1/App.java
+++ Provas IFBA/2013-1/App.java
@@ -3,8 +3,8 @@ public class App{
     
     public void run(){
         ElementoQuimico oxigenio1 = new ElementoQuimico(8, 16, "Oxigenio");
-        ElementoQuimico oxigenio1 = new ElementoQuimico(8, 16, "Oxigenio");
-        ElementoQuimico oxigenio1 = new ElementoQuimico(8, 16, "Oxigenio");
+        ElementoQuimico oxigenio2 = new ElementoQuimico(8, 16, "Oxigenio");
+        ElementoQuimico oxigenio3 = new ElementoQuimico(8, 16, "Oxigenio");
         system.out.println(oxigenio1);
     }
 
