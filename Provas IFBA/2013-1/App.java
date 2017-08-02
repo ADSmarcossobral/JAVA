6d892025d9821ed2465835ae3e4249449d106b7a
aluno <aluno@SSA-D-CPIL03-04.intranet.cefetba.br>
2017-08-01 21:00:52 -0300

    up

--- Provas IFBA/2013-1/App.java
+++ Provas IFBA/2013-1/App.java
@@ -5,7 +5,20 @@ public class App{
         ElementoQuimico oxigenio1 = new ElementoQuimico(8, 16, "Oxigenio");
         ElementoQuimico oxigenio2 = new ElementoQuimico(8, 16, "Oxigenio");
         ElementoQuimico oxigenio3 = new ElementoQuimico(8, 16, "Oxigenio");
-        system.out.println(oxigenio1);
+        ElementoQuimico hidrogenio1 = new ElementoQuimico(1, 2, "Hidrogenio");
+        ElementoQuimico hidrogenio2 = new ElementoQuimico(1, 2, "Hidrogenio");
+        ElementoQuimico hidrogenio3 = new ElementoQuimico(1, 2, "Hidrogenio");
+        ElementoQuimico hidrogenio4 = new ElementoQuimico(1, 2, "Hidrogenio");
+        ElementoQuimico enxofre1 = new ElementoQuimico(16, 32, "Enxofre");
+        
+        Molecula h2o = new Molecula();
+        Molecula h2o2 = new Molecula();
+        Molecula h2so4 = new Molecula();
+
+        h2o.addAtomo(oxigenio1);
+        h2o.addAtomo(hidrogenio1);
+        h2o.addAtomo(hidrogenio2);
+
     }
 
     public static void main(String args[]){
