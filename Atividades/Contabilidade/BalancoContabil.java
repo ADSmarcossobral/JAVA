90995a3a94775b8217cdcb9df4d7a92c97841f08
unknown <Aluno@SSA-D-CPIL03-03.intranet.cefetba.br>
2017-08-21 20:18:16 -0300

    up

--- Atividades/Contabilidade/BalancoContabil.java
+++ Atividades/Contabilidade/BalancoContabil.java
@@ -1,6 +1,14 @@
 
 public class BalancoContabil{
 
-    private Conta[] contas;
+    private Conta[] conta;
+
+    public Conta getConta(int posicao){
+        return this.conta[posicao];
+    }
+
+    public void setConta(int posicao, Conta conta){
+        this.conta[posicao] = conta;
+    }
 
 }
