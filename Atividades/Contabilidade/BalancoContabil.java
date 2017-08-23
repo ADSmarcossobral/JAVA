2a4049bfe32985168b4ff930acf17008f17a841b
ADSmarcossobral94 <marcossobral94@gmail.com>
2017-08-23 17:17:41 -0300

    up

--- Atividades/Contabilidade/BalancoContabil.java
+++ Atividades/Contabilidade/BalancoContabil.java
@@ -1,18 +1,30 @@
 
 public class BalancoContabil{
 
-    private Conta[] conta;
+    private Conta[] contas;
 
     public BalancoContabil(){
         conta = new Conta[0];
     }
 
     public void addConta(Conta conta){
-        Conta[] temp = new Conta[this.conta.length + 1];
-        for(int iConta = 0; iConta < this.conta.length; iConta++)
-            temp[iConta] = this.conta[iConta];
+        Conta[] temp = new Conta[this.contas.length + 1];
+        for(int iConta = 0; iConta < this.contas.length; iConta++)
+            temp[iConta] = this.contas[iConta];
         temp[temp.length - 1] = conta;
-        this.conta = temp;
+        this.contas = temp;
+    }
+
+    public Conta buscarContaPeloNome(String nome){
+        for(Conta atual : this.contas){
+            if(atual.getNome().equals(nome))
+                return atual;
+        }
+    }
+
+    public ajustar(String nomeContaDebito, String nomeContaCredito, double valor){
+        this.buscarContaPeloNome(nomeContaDebito).debitar(valor);
+        this.buscarContaPeloNome(nomeContaCredito).creditar(valor);
     }
 
     public Conta getConta(int posicao){
--- Atividades/Contabilidade/RegistroContabil.java
+++ Atividades/Contabilidade/RegistroContabil.java
@@ -6,7 +6,18 @@ public class RegistroContabil{
     private LivroLancamentoContabil livroLancamentoContabil;
 
     public RegistroContabil(String nomeEmpresa, String CGC, BalancoContabil balancoContabil, LivroLancamentoContabil livroLancamentoContabil){
-        this(nomeEmpresa, CGC, balancoContabil, livroLancamentoContabil);
+        this.setNomeEmpresa(nomeEmpresa);
+        this.setCGC(CGC);
+        this.setBalancoContabil(balancoContabil);
+        this.setLivroLancamentoContabil(livroLancamentoContabil);
+    }
+
+    public void registrarFatoContabil(String descricao, String nomeContaDebito, String nomeContaCredito, double valor){
+        balancoContabil.ajustar(nomeContaCredito, nomeContaDebito, valor);
+        Conta credito = balancoContabil.buscarPeloNome(nomeContaCredito);
+        Conta debito = balancoContabil.buscarPeloNome(nomeContaDebito);
+        LancamentoContabil fato = new LancamentoContabil(descricao, valor, credito, debito);
+        livroLancamentoContabil.addLancamento(fato);
     }
 
     public String getNomeEmpresa(){
